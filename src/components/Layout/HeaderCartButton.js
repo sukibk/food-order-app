a127430d0ed014a25a2a0757d17b448ae395ed26
Marko Sudar <markosudar02@gmail.com>
2023-02-21 23:05:59 -0500

    Cart Button Animation And Counter Enabled

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,42 @@
 import styles from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 const HeaderCartButton = (props) => {
+    const [btnIsHighlited, setBtnIsHighlited] = useState(false);
     const ctx = useContext(CartContext);
+
+    const { items } = ctx;
+
+    const numberOfItemsInCart = items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0);
+
+    const btnStyles = `${styles.button} ${btnIsHighlited && styles.bump}`
+
+    useEffect(() => {
+        if(items.length === 0) return;
+
+        setBtnIsHighlited(true);
+
+        const animationTimeout = setTimeout(()=>{
+            setBtnIsHighlited(false)
+        }, 100)
+
+        // return () => {
+        //     clearTimeout(animationTimeout);
+        // }
+
+    }, [items])
+
   return (
-      <button className={styles.button}>
+      <button className={btnStyles}>
         <span className={styles.icon}>
             <CartIcon />
         </span>
         <span>Your Cart</span>
-        <span className={styles.badge}>{ctx.items.length}</span>
+        <span className={styles.badge}>{numberOfItemsInCart}</span>
       </button>
   );
 };
